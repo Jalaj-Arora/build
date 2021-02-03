c87034b94f6be34632a80bc1c2978257f73660c8
Jalaj Arora <jalaj.arora@harbingergroup.com>
2021-02-03 23:36:19 +0530

    dev

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e80f58f59f7cf6f7de7eaa96c5ec04c6.js"
+  "/precache-manifest.7532851f0888e95309d50157083521b4.js"
 );
 
 self.addEventListener('message', (event) => {
